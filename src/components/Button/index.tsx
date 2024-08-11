c8906a1300b84887884fbdc5cbd4985e4b030430
rudy2steiner <rudy_steiner@613.com>
2024-08-12 00:20:09 +0800

    ts

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,12 +4,12 @@ import styled from 'styled-components';
 
 const Button: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = styled.button`
   align-self: stretch;
-  border-radius: 5px;
-  height: 50px;
+  width: 100px;
   color: #fff;
-  margin: 30px 0 0 0;
-  border: 2px solid #eb9932;
   background-color: #eb9932;
+  &:hover {
+     background-color: #B45309;
+   }
 `;
 
 export default Button;
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,19 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 15px;
   background: #4a4a4a;
-  border: 2px solid #4a4a4a;
+  border: 1px solid #FDBA74;
   display: flex;
   align-items: center;
 
   svg {
     margin-right: 10px;
+    margin-left: 10px;
   }
 `;
 
 export const Input = styled.input`
-  background: #4a4a4a;
-  color: #fff;
+  background: #fff;
+  color: #4a4a4a;
   border: none;
+  text-align: center;
 `;
